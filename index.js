08b6f52507fbb9df72694bb6916ea9663ae223cd
Aayush <aayushsharma47163@gmail.com>
2024-01-19 21:58:57 +0530

    fixed bug

--- index.js
+++ index.js
@@ -38,6 +38,6 @@ app.get('/api/:date', (req,res)=> {
 })
 
 
-var listener = app.listen(process.env.PORT, function () {
+var listener = app.listen(3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
